188bdadf3946a55bb80571083c34069b40051e00
Bankin93 <igorkonov1993@gmail.com>
2023-02-16 12:42:15 +0300

    add main.py and fixed youtube.py

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -5,23 +5,16 @@ from googleapiclient.discovery import build
 
 
 class Youtube:
+    """Базовый класс, для работы с API YouTube"""
     def __init__(self, channel_id):
         self.channel_id = channel_id
 
-        #Получаем ключ API из переменной окружения
-        api_key: str = os.environ.get('YOUTUBE_API_KEY')
-        #сервис youtube
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        #Получаем информацию о канале
-        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        api_key: str = os.environ.get('YOUTUBE_API_KEY')  # Получаем ключ API из переменной окружения
 
-# channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
+        youtube = build('youtube', 'v3', developerKey=api_key)  # сервис youtube
+
+        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()  # инфо о канале
 
     def print_info(self):
         """ Выводим информацию о канале на консоль"""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
-
-
-vdud = Youtube('UCMCgOm8GZkHp8zJ6l7_hIuA')
-vdud.print_info()
-
